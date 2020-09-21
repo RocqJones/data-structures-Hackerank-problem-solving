e429272a1f4a3cabe2090702983fd4a78ff9184a
Matlab Risk <intoverflown@localhost.localdomain>
2020-09-21 19:44:31 +0300

    dynamic array WARNING: bugs

--- 1.arrays/dynamic-arr.py
+++ 1.arrays/dynamic-arr.py
@@ -7,6 +7,30 @@ import sys
 
 def dynamicArray(n, queries):
     # Write your code here
+    last_ans = 0
+    a = []
+    # arr_result = []
+
+    for i in range(n):
+        a.append([])
+
+    for j in queries:
+        x,y = j[1], j[2]
+        if j[1] == 1:
+            sequence = (x ^ last_ans) % n
+            a[sequence].append(y)
+
+        elif j[2]  == 2:
+            sequence = (x ^ last_ans) % n
+            size = y % len(a[sequence])
+            vals = a[sequence][size]
+            # arr_result.append(last_ans)
+            last_ans = vals
+            
+
+    return last_ans
+    # print(arr_result)
+    # print(last_ans)
 
 if __name__ == '__main__':
     first_multiple_input = input().rstrip().split()
@@ -22,3 +46,4 @@ if __name__ == '__main__':
 
     dynamicArray(n, queries)
     # result = dynamicArray(n, queries)
+    # print(result)
